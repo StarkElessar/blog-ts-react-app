33f99fb189bfd86759d26a02661be7363165b0be
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2024-01-10 08:15:13 +0100

    fix: BLOG-33 last changes by comments

--- src/utils/mock-data/index.ts
+++ src/utils/mock-data/index.ts
@@ -1,24 +1,26 @@
 import { ICategories, IRecentPost } from '../../types';
 import postImage from '../../assets/images/recent-post-items/01.jpg';
-import { getId } from '../index';
+// import { getId } from '../index';
+
+
 
 export const RECENT_POSTS: IRecentPost[] = [
   {
-    id: getId(),
+    id: crypto.randomUUID(),
     number: 1,
     image: postImage,
     title: 'The spectacle before us was indeed sublime',
     date: new Date('9.26.2019'),
   },
   {
-    id: getId(),
+    id: crypto.randomUUID(),
     number: 2,
     image: postImage,
     title: 'Far far away, behind the word mountains',
     date: new Date('11.9.2023'),
   },
   {
-    id: getId(),
+    id: crypto.randomUUID(),
     number: 3,
     image: postImage,
     title: 'Musical improvisation is the spontaneous music',
@@ -28,10 +30,10 @@ export const RECENT_POSTS: IRecentPost[] = [
 
 // тестовые данные
 export const CATEGORIES: ICategories[] = [
-  { categoryName: 'Health', color: '#83ea6d', id: getId() },
-  { categoryName: 'Music', color: '#84b2f4', id: getId() },
-  { categoryName: 'Lifestyle', color: '#ffd001', id: getId() },
-  { categoryName: 'Health', color: '#fa0899', id: getId() },
-  { categoryName: 'Technology', color: '#c4c5fe', id: getId() },
-  { categoryName: 'Nature', color: '#48dfd4', id: getId() },
+  { categoryName: 'Health', color: '#83ea6d', id: crypto.randomUUID() },
+  { categoryName: 'Music', color: '#84b2f4', id: crypto.randomUUID() },
+  { categoryName: 'Lifestyle', color: '#ffd001', id: crypto.randomUUID() },
+  { categoryName: 'Health', color: '#fa0899', id: crypto.randomUUID() },
+  { categoryName: 'Technology', color: '#c4c5fe', id: crypto.randomUUID() },
+  { categoryName: 'Nature', color: '#48dfd4', id: crypto.randomUUID() },
 ];
