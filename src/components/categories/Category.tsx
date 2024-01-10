9a9f0efb33145facb8ec7b8297c43fc63d2fa7b1
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2024-01-10 07:42:23 +0100

    fix: BLOG-33 changes by comments

--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -8,7 +8,7 @@ export const Category: FC<ICategories> = ({ color, categoryName, id }) => {
   return (
     <Link to={`/category/${id}`} key={id} className={'category button-un-hover'}>
       <span className={'category__icon'}><HexagonIcon color={color} /></span>
-      <span className={'category__name'}>{`${categoryName}`}</span>
+      <span className={'category__name'}>{categoryName}</span>
     </Link>
   );
 };
